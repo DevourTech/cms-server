59394a6070047b486d6d891bf3341bdf9358edb3
Archit Singh <architsingh456@gmail.com>
2021-01-25 23:38:07 +0530

    init : added dummy rest API

---
+++ src/main/java/org/cms/ServerApplication.java
@@ -0,0 +1,14 @@
+package org.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
